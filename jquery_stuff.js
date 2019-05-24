9d97f6c8d5b95acad67da705358ef036d3efac24
Junaid Aslam <junaid.aslam@cppa.gov.pk>
2019-05-24 12:15:13 +0500

    Update jquery_stuff.js
    Now it shows data as we have removed some errors and used variables to append them to div.

--- jquery_stuff.js
+++ jquery_stuff.js
@@ -10,26 +10,36 @@ $(document).ready(function(){
 	});
 	
 	$(".btn_submit").click(function(){
-		alert($("firstname").text());
+
+		var firstname=$("input[name='firstname']").val();
+		var lastname=$("input[name='lastname']").val();
+		var gender=$("input[type='radio']").val();
+		var religion=$("select[name='Religion']").val();
+		var profession=$("select[name='Profession']").val();
+		var experience=$("input[type='number']").val();
+		
+		$("form").empty();
+		
 		$(".showData").append(
-		"<b>First Name: <b>"+$("firstname").text()+
-		"<br><b>Last Name: <b>"+$("lastname").text()+
-		"<br><b>Gender: <b>"+$("input[type='radio']").text()+
-		"<br><b>Religion: <b>"+$("select[name='Religion']").text()+
-		"<br><b>Profession: <b>"+$("select[name='Profession']").text()+
-		"<br><b>Experience: <b>"+$("input[type='number']").text());
+		"<b>First Name: <b>"+ firstname+
+		"<br><b>Last Name: <b>"+ lastname+
+		"<br><b>Gender: <b>"+ gender+
+		"<br><b>Religion: <b>"+ religion+
+		"<br><b>Profession: <b>"+ profession+
+		"<br><b>Experience: <b>"+ experience+ " years"); 
+		
 		$("a[name='help']").text("Congrats!");
-		$("form").hide();
+		
 	});
 	
 	$(".btn_animated").click(function(){
 		// $(".showData").append();
 		$(".animated1")
-		.css("color", "red")
+		.css("background-color", "red")
 		.css("width","200px")
 		.css("height","200px")
-		.animate({left: '250px'})
-		.fadeIn("slow")
-		.animate({right: '250px'});
+		.fadeOut("slow");
+		
+		//$(".animated1").animate({right: '250px'});
 	});
 });	
